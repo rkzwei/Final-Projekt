d7a90f3041c7838a54c0a862f6b0926a4c14191b
Gabriel Neves <rkzwei@rkzwei.local>
2022-12-30 16:20:47 -0300

    Overall changes

--- testers.py
+++ testers.py
@@ -1,12 +1,12 @@
 #TODO add support for other manufacturers
-
+from os import system
 import math
 import aclist
 import helpers
 import sqlite3
 
 
-print("Weight and Balance Calculator\n\n")
+""" print("Weight and Balance Calculator\n\n")
 try:
     conn = sqlite3.connect("./databases/aircraft-ledger.db")
     cur = conn.cursor()
@@ -17,6 +17,32 @@ manufacturer =(helpers.sqllist["manufacturer"])
 
 query = cur.execute(manufacturer)
 
-print([result[0].capitalize() for result in query])
+print([result[0].capitalize() for result in query]) """
+
+
+""" print([result[0].capitalize() for result in helpers.statement("aircraft-ledger", "manufacturer")]) """
+DATA = {
+    0:"aircraft-ledger",
+    1:"piper",
+    2:"cessna",
+    3:"diamond"
+}
+
+make = list(result[0].capitalize() for result in helpers.statement(DATA[0], "manufacturer"))
+
+while True:
+    try:
+        print("\n",make,"\n")
+        selection= input("Type choice from one from the above: ")
+        selection.lower()
+        if selection.capitalize() in make:
+            break
+        else:
+            system('clear')
+            print("\nInvalid option.\n")
+    except Exception as x:
+        print(x)
+        break
+
 
 
